8170b00fb3c50dbfcac9ffe83703f9e8dd7d6f24
Ashley Qian <ashley.qian@duke.edu>
2014-01-16 00:10:41 -0500

    woohoo random color generated wolf enemies

--- src/mygame/MyGame.java
+++ src/mygame/MyGame.java
@@ -1,13 +1,34 @@
-
 package mygame;
 import jgame.*;
 import jgame.platform.*;
-/** Minimal shooter illustrating Eclipse usage. */
+
 public class MyGame extends StdGame {
-	public static void main(String[]args) {new MyGame(parseSizeArgs(args,0));}
-	public MyGame() { initEngineApplet(); }
-	public MyGame(JGPoint size) { initEngine(size.x,size.y); }
-	public void initCanvas() { setCanvasSettings(32,24,8,8,null,null,null); }
+
+	int key_shoot = 70;
+	int key_blue = 68;
+	int key_green = 83;
+	int key_red = 65;
+	
+	int red = 0;
+	int green = 0;
+	int blue = 0;
+
+	public static void main(String[]args) {
+		new MyGame(parseSizeArgs(args,0));
+	}
+
+	public MyGame() {
+		initEngineApplet();
+	}
+
+	public MyGame(JGPoint size) {
+		initEngine(size.x,size.y);
+	}
+
+	public void initCanvas() {
+		setCanvasSettings(40, 10, 8, 8, null, null, null);
+	}
+
 	public void initGame() {
 		defineMedia("mygame.tbl");
 		if (isMidlet()) {
@@ -16,65 +37,83 @@ public class MyGame extends StdGame {
 		} else {
 			setFrameRate(45,1);
 		}
-		setHighscores(10,new Highscore(0,"nobody"),15);
-		startgame_ingame=true;
+		startgame_ingame = true;
 	}
+
 	public void initNewLife() {
 		removeObjects(null,0);
-		new Player(pfWidth()/2,pfHeight()-32,5);
+		new Player(10, pfHeight() - 10, 5);
 	}
-	public void startGameOver() { removeObjects(null,0); }
+
+	public void startGameOver() {
+		removeObjects(null,0);
+	}
+
 	public void doFrameInGame() {
 		moveObjects();
-		checkCollision(2,1); // enemies hit player
-		checkCollision(4,2); // bullets hit enemies
-		if (checkTime(0,(int)(800),(int)((12-level/2))))
-			new Enemy();
-		if (gametime>=800 && countObjects("enemy",0)==0) levelDone();
-	}
-	public void incrementLevel() {
-		score += 50;
-		if (level<7) level++;
-		stage++;
-	}
-	JGFont scoring_font = new JGFont("Arial",0,8);
-	public class Enemy extends JGObject {
-		double timer=0;
-		public Enemy() {
-			super("enemy",true,random(32,pfWidth()-40),-8,
-					2, stage%2==1 ? "block" : "ball",
-					random(-1,1), (1.0+level/2.0), -2 );
+		checkCollision(2,1);
+		if (checkTime(0, 800, (int) random(25, 75)))						// randomly generates a wolf within the level
+			new Wolf(); 													// wolves get produced faster as levels increase
+		if (gametime >= 800 && countObjects("wolf", 0) == 0) {				// gametimes goes for 800
+			levelDone();
 		}
+	}
+
+	public void incrementLevel() {
+	}
+
+	JGFont scoring_font = new JGFont("Arial",0,8);
+
+	public class Wolf extends JGObject {
+		int[] color = {255 * (int)random(0,1,1), 255 * (int)random(0,1,1), 255 * (int)random(0,1,1)};
+		public Wolf() {
+			super("wolf", true, pfWidth() - 10, pfHeight() - 10, 2, stage%2==1 ? "block" : "ball", random(-1,1), 0, -2 );
+		}
+
+		public void paint() {
+			setColor(new JGColor(color[0], color[1], color[2]));
+			drawRect(x, y, 16, 16, true, true);
+		}
+
 		public void move() {
 			timer += gamespeed;
-			x += Math.sin(0.1*timer);
-			y += Math.cos(0.1*timer);
-			if (y>pfHeight()) y = -8;
+			if (getKey(key_shoot)) {
+				hit(this);
+			}
+			x -= 1;
 		}
+
 		public void hit(JGObject o) {
 			remove();
 			o.remove();
 			score += 5;
 		}
 	}
+
 	public class Player extends JGObject {
-		public Player(double x,double y,double speed) {
-			super("player",true,x,y,1,"shipu", 0,0,speed,speed,-1);
+		public Player(double x, double y, double speed) {
+			super("woolson", true, x, y, 1, "shipu", 0, 0, speed, speed, -1);
 		}
+		
+		public void paint() {
+			setColor(new JGColor(red, blue, green));
+			drawRect(x, y, 16, 16, true, true);
+		}
+
 		public void move() {
 			setDir(0,0);
-			if (getKey(key_left)  && x > xspeed)               xdir=-1;
-			if (getKey(key_right) && x < pfWidth()-32-yspeed)  xdir=1;
-			if (getKey(key_fire) && countObjects("bullet",0) < 2) {
-				new JGObject("bullet",true,x,y,4,"bary", 0,-5, -2);
-				clearKey(key_fire);
+			if (getKey(key_left) && x > xspeed){
+				xdir = -1;
+			}
+			if (getKey(key_right) && x < pfWidth()-32-yspeed){
+				xdir = 1;
 			}
 		}
-		public void hit(JGObject obj) {
-			if (and(obj.colid,2)) lifeLost();
-			else {
-				score += 5;
-				obj.remove();
+
+		public void hit(JGObject o) {
+			System.out.print("AJKSLD");
+			if (and(o.colid, 2)){
+				gameOver();
 			}
 		}
 	}
