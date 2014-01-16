885cd54e1505218cea9b3e41433d1f4e9863aa97
Ashley Qian <ashley.qian@duke.edu>
2014-01-16 12:49:39 -0500

    MOAR COLORS FOR ALL WOLVES AND WOOLSON TOO

--- src/mygame/MyGame.java
+++ src/mygame/MyGame.java
@@ -55,10 +55,22 @@ public class MyGame extends StdGame {
 	public void doFrameInGame() {
 		moveObjects();
 		checkCollision(2,1);
-		if (getKey(key_red)){
-			System.out.println("THIS HAS BEEN DONE");
-			red = 255;
-			new Player(playerX, playerY, 5);
+		if (getKey(key_up) || getKey(key_down)){									// make sure you have dark (up) or light (down)
+			if (getKey(key_red) || getKey(key_green) || getKey(key_blue)){			// make sure you're hitting either red, green or blue
+				
+				int boolRed = getKey(key_red) ? 1 : 0;								// get the integer values of the booleans for color rendering
+				int boolGreen = getKey(key_blue) ? 1 : 0;
+				int boolBlue = getKey(key_green) ? 1 : 0;
+				int boolDark = getKey(key_up) ? 1 : 0;
+				
+				red = (boolRed * 255) - (boolRed * boolDark * 100); 				// if the dark key is hit then woolson turns into a darker color
+				green = (boolGreen * 255) - (boolGreen * boolDark * 100);
+				blue = (boolBlue * 255) - (boolBlue * boolDark * 100);
+				
+				System.out.println("red " + red + " green " + green + " blue " + blue);
+				new Player(playerX, playerY, 5);
+			}
+			
 		}
 		if (checkTime(0, 800, (int) random(25, 75)))						// randomly generates a wolf within the level
 			new Enemy(); 													// wolves get produced faster as levels increase
@@ -73,9 +85,19 @@ public class MyGame extends StdGame {
 	JGFont scoring_font = new JGFont("Arial",0,8);
 
 	public class Enemy extends JGObject {
-		int[] color = {255 * (int)random(0,1,1), 255 * (int)random(0,1,1), 255 * (int)random(0,1,1)};
+		int[] color = {							// initialize colors
+				255 * (int)random(0,1,1),
+				255 * (int)random(0,1,1),
+				255 * (int)random(0,1,1)
+				};
+		
 		public Enemy() {
 			super("wolf", true, pfWidth() - 10, pfHeight() - 10, 2, stage%2==1 ? "block" : "ball", random(-1,1), 0, -2 );
+			for (int i = 0; x < color.length; i++){		// initlalize darker colors
+				if (color[i] == 255) {
+					color[i] = color[i] - (100 * (int)random(0,1,1));
+				}
+			}
 		}
 
 		public void paint() {
@@ -103,9 +125,7 @@ public class MyGame extends StdGame {
 			super("woolson", true, x, y, 1, "shipu", 0, 0, speed, speed, -1);
 		}
 		
-		public void paint() {
-			System.out.println("I HAVE BEEN PAINTED " + red + " " + blue + " " + green);
-			
+		public void paint() {			
 			setColor(new JGColor(red, blue, green));
 			drawOval(x, y, 16, 16, true, true);
 		}
